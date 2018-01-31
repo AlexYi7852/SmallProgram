a73134d2b76f9ae3e97a8c29f64e606fc3e3780d
AlexYi7852 <3092342855@qq.com>
2018-01-31 22:06:57 +0800

    add Storage 用法

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -6,7 +6,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+
+  },
+
+  onCollectionTap: function (event) {
+    var key = wx.getStorageSync('key')
+  },
+
+  onShareTap: function (event) {
+    wx.removeStorageSync("key")
   },
 
   /**
@@ -15,54 +23,57 @@ Page({
   onLoad: function (options) {
     var postId = options.id
     this.setData({ data: postsData.postList[postId] })
+    // 缓存的上限不能超过10MB
+    wx.setStorageSync("key", "生命周期函数")
+    wx.setStorageSync("esion", "人来人往")
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
