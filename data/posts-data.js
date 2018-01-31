f81a25af2fe81ca84f081054c6cb37df9fec4e49
AlexYi7852 <3092342855@qq.com>
2018-01-31 09:43:02 +0800

    project start

---
+++ data/posts-data.js
@@ -0,0 +1,66 @@
+var local_database = [
+  {
+    date: "Nov 11 1111",
+    title: "傾城",
+    imgSrc: "/images/post/crab.png",
+    avatar: "/images/avatar/1.png",
+    content: "熱情就算熄滅了，分手這一晚也重要",
+    reading: "11",
+    collection: "1111",
+    postId: 0
+  },
+  {
+    date: "Sep 12 2222",
+    title: "十年",
+    imgSrc: "/images/post/bl.png",
+    avatar: "/images/avatar/2.png",
+    content: "甜言蜜語謊話嬉笑，多給我一點切勿缺少",
+    reading: "22",
+    collection: "2222",
+    postId: 1
+  },
+  {
+    date: "Nov 13 3333",
+    title: "失憶蝴蝶",
+    imgSrc: "/images/post/cat.png",
+    avatar: "/images/avatar/3.png",
+    content: "話題勁了也不緊要，吻我至淒冷的神效",
+    reading: "33",
+    collection: "3333",
+    postId: 2
+  },
+  {
+    date: "Sep 14 4444",
+    title: "明年今日",
+    imgSrc: "/images/post/sls.jpg",
+    avatar: "/images/avatar/4.png",
+    content: "芳華鬧市燈光普照，然而共你已再沒破曉",
+    reading: "44",
+    collection: "4444",
+    postId: 3
+  },
+  {
+    date: "Nov 15 5555",
+    title: "人來人往",
+    imgSrc: "/images/post/vr.png",
+    avatar: "/images/avatar/5.png",
+    content: "紅眼睛幽幽的看著這孤城，如同苦笑擠出的高興",
+    reading: "55",
+    collection: "5555",
+    postId: 4
+  },
+  {
+    date: "Sep 16 6666",
+    title: "最冷一天",
+    imgSrc: "/images/post/xiaolong.jpg",
+    avatar: "/images/avatar/2.png",
+    content: "全城為我花光狠勁，浮華盛世作分手佈景",
+    reading: "66",
+    collection: "6666",
+    postId: 5
+  }
+]
+
+module.exports = {
+  postList: local_database
+}
