c15ab0ac996bac98f48462c88993eecc4ff3acbe
AlexYi7852 <3092342855@qq.com>
2018-02-08 23:09:45 +0800

    在于

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,3 +1,36 @@
-Page({
+var app = getApp()
 
+Page({
+  // RESTFul API JSON
+  // SOAP XML
+
+  onLoad: function(event){
+    var doubanBase = app.globalData.doubanBase
+    var dataCount = "?start=0&count=3"
+    var inTheatersURL = doubanBase + "/v2/movie/in_theaters" + dataCount // 正在热映
+    var comingSoonURL = doubanBase + "/v2/movie/coming_soon" + dataCount // 即将上映
+    var top250URL = doubanBase + "/v2/movie/top250" + dataCount
+    this.getMovieListData(inTheatersURL)
+    this.getMovieListData(comingSoonURL)
+    this.getMovieListData(top250URL)
+  },
+
+  getMovieListData: function(url){
+    wx.request({
+      url: url,
+      method: "GET", // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      // 设置请求的header
+      header: {
+        "Content-Type": "application/json"
+      },
+      success: function (res) {
+        // success
+        console.log(res)
+      },
+      fail: function (res) {
+        // fail
+        console.log('failed')
+      }
+    })
+  }
 })
