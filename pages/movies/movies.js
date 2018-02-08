42fe06407a301587a16daf5c3b96ff7291b8777e
AlexYi7852 <3092342855@qq.com>
2018-02-08 23:48:14 +0800

    成功渲染了数据

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -11,11 +11,12 @@ Page({
     var comingSoonURL = doubanBase + "/v2/movie/coming_soon" + dataCount // 即将上映
     var top250URL = doubanBase + "/v2/movie/top250" + dataCount
     this.getMovieListData(inTheatersURL)
-    this.getMovieListData(comingSoonURL)
-    this.getMovieListData(top250URL)
+    // this.getMovieListData(comingSoonURL)
+    // this.getMovieListData(top250URL)
   },
 
   getMovieListData: function(url){
+    var that = this
     wx.request({
       url: url,
       method: "GET", // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
@@ -25,12 +26,33 @@ Page({
       },
       success: function (res) {
         // success
-        console.log(res)
+        that.processDoubanData(res.data.subjects)
       },
       fail: function (res) {
         // fail
         console.log('failed')
       }
+    })
+  },
+
+  processDoubanData: function(moviesDouban){
+    var movies = []
+    for (var index in moviesDouban){
+      var subject = moviesDouban[index]
+      var title = subject.title
+      if (title.length >= 6){
+        title = title.substring(0, 6) + "..."
+      }
+      var temp = {
+        title: title,
+        average: subject.rating.average,
+        coverageUrl: subject.images.large,
+        movieId: subject.id
+      }
+      movies.push(temp)
+    }
+    this.setData({
+      movies: movies
     })
   }
 })
