e4fecd20b742d4513bb3db14a35380acdde40047
AlexYi7852 <3092342855@qq.com>
2018-02-11 21:17:53 +0800

    使用Module和Class提取一个Movie对象

---
+++ pages/movies/movie-detail/class/Movie.js
@@ -0,0 +1,49 @@
+var util = require('../../../../utils/utils.js')
+
+class Movie {
+  constructor(url) {
+    this.url = url
+  }
+
+  getMovieData(callBack) {
+    this.callBack = callBack
+    console.log(this)
+    util.getMovieListData(this.url, this.processDoubanData.bind(this))
+  }
+
+  processDoubanData(data) {
+    // 导演
+    if (!data) { return }
+    var director = {
+      avatar: "",
+      name: "",
+      id: ""
+    }
+    if (data.directors[0] != null) {
+      if (data.directors[0].avatars != null) {
+        director.avatar = data.directors[0].avatars.large
+      }
+      director.name = data.directors[0].name
+      director.id = data.directors[0].id
+    }
+    var movie = {
+      movieImg: data.images ? data.images.large : "",  // 电影海报
+      country: data.countries[0],  // 国家
+      title: data.title, // 片名
+      originalTitle: data.original_title, // 原名
+      wishCount: data.wish_count, // 多少人想看
+      commentCount: data.comments_count, // 评论数
+      year: data.year,
+      genres: data.genres.join("、"),  // 题材
+      stars: util.convertToStarsArray(data.rating.stars),  // 评分
+      score: data.rating.average,  // 评分
+      director: director, // 导演
+      casts: util.convertToCastString(data.casts),  // 演员表
+      castsInfo: util.convertToCastInfos(data.casts), // 演员表信息
+      summary: data.summary // 剧情简介
+    }
+    this.callBack(movie)
+  }
+}
+
+export { Movie }
--- pages/movies/movie-detail/movie-detail.js
+++ pages/movies/movie-detail/movie-detail.js
@@ -1,5 +1,6 @@
+
+import { Movie } from "class/Movie.js";
 var app = getApp()
-var util = require('../../../utils/utils.js')
 Page({
 
   /**
@@ -19,43 +20,13 @@ Page({
   onLoad: function (options) {
     var movieId = options.id
     var url = app.globalData.doubanBase + "/v2/movie/subject/" + movieId
-    util.getMovieListData(url, this.processDoubanData)
+    var movie = new Movie(url)
+    var that = this
+    movie.getMovieData(function(movie){
+      that.setData({ movie: movie })
+    })
   },
 
-  processDoubanData: function(data){
-    // 导演
-    console.log(data)
-    var director = {
-      avatar: "",
-      name: "",
-      id: ""
-    }
-    if(data.directors[0] != null){
-      if (data.directors[0].avatars != null){
-        director.avatar = data.directors[0].avatars.large
-      }
-      director.name = data.directors[0].name
-      director.id = data.directors[0].id
-    }
-    var movie = {
-      movieImg: data.images ? data.images.large : "",  // 电影海报
-      country: data.countries[0],  // 国家
-      title: data.title, // 片名
-      originalTitle: data.original_title, // 原名
-      wishCount: data.wish_count, // 多少人想看
-      commentCount: data.comments_count, // 评论数
-      year: data.year,
-      genres: data.genres.join("、"),  // 题材
-      stars: util.convertToStarsArray(data.rating.stars),  // 评分
-      score: data.rating.average,  // 评分
-      director: director, // 导演
-      casts: util.convertToCastString(data.casts),  // 演员表
-      castsInfo: util.convertToCastInfos(data.casts), // 演员表信息
-      summary: data.summary // 剧情简介
-    }
-    this.setData({ movie: movie })
-    console.log(this.data.movie)
-  },
   // 查看图片
   viewMoviePostImg: function(e){
     var src = e.currentTarget.dataset.src
